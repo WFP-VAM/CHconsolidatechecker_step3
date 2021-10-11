8c493c871c82789c641de3df30a62132270f165b
olanderb <billyolander@gmail.com>
2021-10-11 13:31:08 +0000

    Update runoffline.R

--- runoffline.R
+++ runoffline.R
@@ -1,4 +1,4 @@
-
 #this will run shiny app from github repository https://github.com/WFP-VAM/CHconsolidatechecker_step3 on local machine
+library(shiny)
 
 runUrl('https://github.com/WFP-VAM/CHconsolidatechecker_step3/archive/main.tar.gz')
